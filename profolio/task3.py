c8a885d16d2fa944d10f8aee3037cdcbaaeca4ce
Salim Medani <salimmedani@Salims-MBP.lan>
2023-03-10 17:05:18 -0600

    first commit

---
+++ profolio/task3.py
@@ -0,0 +1,52 @@
+#question 1
+#writ a foction  to print "hello_USERNAME!" USERNAME is the input of the function . the first lin of the code has been definedas below.
+
+def hello_name(user_name):
+    print("hello_" + user_name + "!")
+
+
+
+ #question 2
+#writ a python functino ,first_odds that print the odd numbers from 1-100 and returns nothing
+
+def  first_odds():
+    for num in range(1 ,100):
+      print(num)
+
+
+#question 3
+#Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
+
+def max_num_in_list(a_list):
+
+
+
+
+
+
+#question 4
+#Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+
+   def  is_leap_year(a_year):
+    if a_year % 4 == 0:  
+      if a_year % 100 == 0: 
+         if a_year % 400 == 0:
+          return True
+            
+
+# question 5            
+#Write a function to check to see if all numbers in list are consecutive numbers. For example, 
+# [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+
+    def is_consecutive(a_list):
+       if len(a_list)>= 1:
+   else:
+   my_list = my(a_list)
+   
+for i in range(len(a_list)-1):
+        if  my_list[i+1] - my_list[i] +1:
+           r
+            
+      
+      
+       
